2f9dee9e2250490eeb0e02720472bce4df4d78b6
Ivan Shutov <vanasutov793@gmail.com>
2024-09-24 10:15:04 +0400

    refactor(DragNDropItemDirective): разделил ответственность

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,41 @@
 const nx = require('@nx/eslint-plugin');
 
 module.exports = [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
-  ...nx.configs['flat/angular'],
-  ...nx.configs['flat/angular-template'],
-  {
-    files: ['**/*.ts'],
-    rules: {
-      '@angular-eslint/directive-selector': [
-        'error',
-        {
-          type: 'attribute',
-          prefix: 'app',
-          style: 'camelCase',
-        },
-      ],
-      '@angular-eslint/component-selector': [
-        'error',
-        {
-          type: 'element',
-          prefix: 'app',
-          style: 'kebab-case',
-        },
-      ],
+    ...nx.configs['flat/base'],
+    ...nx.configs['flat/typescript'],
+    ...nx.configs['flat/javascript'],
+    {
+        files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+        // Override or add rules here
+        rules: {},
     },
-  },
-  {
-    files: ['**/*.html'],
-    // Override or add rules here
-    rules: {},
-  },
+    ...nx.configs['flat/angular'],
+    ...nx.configs['flat/angular-template'],
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@angular-eslint/directive-selector': [
+                'error',
+                {
+                    type: 'attribute',
+                    prefix: 'app',
+                    style: 'camelCase',
+                },
+            ],
+            '@angular-eslint/component-selector': [
+                'error',
+                {
+                    type: 'element',
+                    prefix: 'app',
+                    style: 'kebab-case',
+                },
+            ],
+            '@typescript-eslint/no-unused-expressions': 'allow',
+        },
+    },
+    {
+        files: ['**/*.html'],
+        // Override or add rules here
+        rules: {},
+    },
 ];
--- src/directives/drag-n-drop/item/drag-n-drop-item.directive.ts
+++ src/directives/drag-n-drop/item/drag-n-drop-item.directive.ts
@@ -18,80 +18,40 @@ export class DragNDropItemDirective implements OnInit {
     private readonly eRef = inject<ElementRef<HTMLElement>>(ElementRef);
     private readonly elem = this.eRef.nativeElement;
     private readonly dndService = inject(DragNDropService);
-    private readonly dndContainer = this.dndService.dndElemContainer;
     private readonly isStartDragEffect = effect(() => {
-        this.eRef.nativeElement.style.cursor = this.isStartDrag()
-            ? 'grabbing'
-            : 'grab';
+        this.elem.style.cursor = this.isStartDrag() ? 'grabbing' : 'grab';
     });
-    private containerRelativeStartPos = {
-        top: 0,
-        left: 0,
-    };
-    private setInitialPos(initialPos: { top: number; left: number }) {
-        this.eRef.nativeElement.style.top = initialPos.top + 'px';
-        this.eRef.nativeElement.style.left = initialPos.left + 'px';
-
-        window.setTimeout(
-            () => (this.eRef.nativeElement.style.position = 'absolute')
-        );
-    }
-    private readonly dndElemContainerEffect = effect(
-        () => {
-            const dndContainerElem =
-                this.dndService.dndElemContainer() as HTMLElement;
-            const dndContainerDomRect =
-                dndContainerElem.getBoundingClientRect();
-
-            if (dndContainerDomRect) {
-                const oldDndContainerHeight =
-                    this.dndContainer()?.getBoundingClientRect().height;
-                const oldDndContainerWidth =
-                    this.dndContainer()?.getBoundingClientRect().width;
-
-                const domRect = this.elem.getBoundingClientRect();
-                const top =
-                    domRect.y -
-                    dndContainerDomRect.y -
-                    parseFloat(getComputedStyle(this.elem).marginTop);
-                const left =
-                    domRect.x -
-                    dndContainerDomRect.x -
-                    parseFloat(getComputedStyle(this.elem).marginLeft);
-
-                this.containerRelativeStartPos = {
-                    top,
-                    left,
-                };
-                this.setInitialPos({
-                    top,
-                    left,
-                });
-                dndContainerElem.style.height = oldDndContainerHeight + 'px';
-                dndContainerElem.style.width = oldDndContainerWidth + 'px';
-            }
-        },
-        { allowSignalWrites: true }
-    );
     private readonly dndSiblingItems = computed(() =>
         this.init()
             ? this.dndService
                   .dndItems()
                   ?.filter(
-                      ({ absoluteInitialPos }) =>
+                      ({ absoluteInitialPosX, absoluteInitialPosY }) =>
                           !(
-                              absoluteInitialPos.x ===
-                                  this.absoluteInitialPos.x &&
-                              absoluteInitialPos.y === this.absoluteInitialPos.y
+                              absoluteInitialPosX ===
+                                  this.absoluteInitialPosX &&
+                              absoluteInitialPosY === this.absoluteInitialPosY
                           )
                   )
             : []
     );
+    private readonly dndElemContainerEffect = effect(
+        () => {
+            const dndContainerElem =
+                this.dndService.dndElemContainer() as HTMLElement;
+
+            dndContainerElem && this.transformInitialSetup(dndContainerElem);
+        },
+        { allowSignalWrites: true }
+    );
     private readonly init = signal(false);
     private readonly isStartDrag = signal(false);
-    private readonly absoluteInitialPos = {
-        x: this.elem.getBoundingClientRect().x,
-        y: this.elem.getBoundingClientRect().y,
+    private readonly absoluteInitialPosX = this.elem.getBoundingClientRect().x;
+    private readonly absoluteInitialPosY = this.elem.getBoundingClientRect().y;
+
+    private containerRelativeStartPos = {
+        top: 0,
+        left: 0,
     };
     private startDragY = 0;
     private startDragX = 0;
@@ -103,13 +63,11 @@ export class DragNDropItemDirective implements OnInit {
     @HostListener('mousedown', ['$event']) private onMousedown(
         event: MouseEvent
     ) {
-        this.startDragY = event.y;
-        this.startDragX = event.x;
-        this.elem.style.zIndex = '999';
-        this.isStartDrag.set(true);
+        this.setDragStartState(event.x, event.y);
     }
 
     //  TODO: с window:mouseup надо чета делать, так как это не правильно(хз)
+    // надо чета делать с тем что 'window:mouseup' вызывается для всех вообще dndItem
     @HostListener('window:mouseup')
     @HostListener('mouseup')
     private onMouseup() {
@@ -121,48 +79,88 @@ export class DragNDropItemDirective implements OnInit {
         event: MouseEvent
     ) {
         if (this.isStartDrag()) {
-            const sibling = this.dndSiblingItems()?.find((item) =>
-                isSufficientCovered(
-                    this.eRef.nativeElement,
-                    item.eRef.nativeElement,
-                    50
-                )
-            );
+            const coveredSibling = this.getCoveredSibling();
 
-            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
-            sibling
-                ? this.switchStartPosition(sibling)
+            coveredSibling
+                ? this.switchStartPosition(coveredSibling)
                 : this.elemOffsetToPoint(event.x, event.y);
         }
     }
 
-    setStartPosition() {
-        this.eRef.nativeElement.style.top =
-            this.containerRelativeStartPos.top + 'px';
-        this.eRef.nativeElement.style.left =
-            this.containerRelativeStartPos.left + 'px';
-        this.eRef.nativeElement.style.zIndex = '0';
-        this.isStartDrag.set(false);
+    private transformInitialSetup(dndContainerElem: HTMLElement) {
+        const dndContainerDomRect = dndContainerElem.getBoundingClientRect();
+        const oldDndContainerHeight = dndContainerDomRect.height;
+        const oldDndContainerWidth = dndContainerDomRect.width;
+
+        this.setContainerRelativeStartPos(dndContainerDomRect);
+        this.setElementPositionToAbsolute();
+
+        dndContainerElem.style.height = oldDndContainerHeight + 'px';
+        dndContainerElem.style.width = oldDndContainerWidth + 'px';
     }
 
-    switchStartPosition(dndItem: DragNDropItemDirective) {
-        const oldContainerRelativeStartPos = this.containerRelativeStartPos;
-        const oldDndItemContainerRelativeStartPos =
-            dndItem.containerRelativeStartPos;
+    private setElementPositionToAbsolute() {
+        this.elem.style.top = this.containerRelativeStartPos.top + 'px';
+        this.elem.style.left = this.containerRelativeStartPos.left + 'px';
 
-        this.containerRelativeStartPos = oldDndItemContainerRelativeStartPos;
-        dndItem.containerRelativeStartPos = oldContainerRelativeStartPos;
+        window.setTimeout(() => (this.elem.style.position = 'absolute'));
+    }
 
+    private setContainerRelativeStartPos(dndContainerDomRect: DOMRect) {
+        const domRect = this.elem.getBoundingClientRect();
+        const top =
+            domRect.y -
+            dndContainerDomRect.y -
+            parseFloat(getComputedStyle(this.elem).marginTop);
+        const left =
+            domRect.x -
+            dndContainerDomRect.x -
+            parseFloat(getComputedStyle(this.elem).marginLeft);
+
+        this.containerRelativeStartPos = {
+            top,
+            left,
+        };
+    }
+
+    private switchStartPosition(dndItem: DragNDropItemDirective) {
+        const newContainerRelativeStartPos = dndItem.containerRelativeStartPos;
+        const newDndItemContainerRelativeStartPos =
+            this.containerRelativeStartPos;
+
+        this.containerRelativeStartPos = newContainerRelativeStartPos;
         this.setStartPosition(); // рабочее решение но с нюансом(оно сразу меняет позиции),
         // но походу это даже лучше, так как можно сделать так, что если чел секунду покрывал элемент,
         // то меням сразу, иначе вообще не меняем, а чел просто дальше идет выбирать
+
+        dndItem.containerRelativeStartPos = newDndItemContainerRelativeStartPos;
         dndItem.setStartPosition();
     }
 
-    elemOffsetToPoint(x: number, y: number) {
-        this.eRef.nativeElement.style.top =
+    private elemOffsetToPoint(x: number, y: number) {
+        this.elem.style.top =
             y + this.containerRelativeStartPos.top - this.startDragY + 'px';
-        this.eRef.nativeElement.style.left =
+        this.elem.style.left =
             x + this.containerRelativeStartPos.left - this.startDragX + 'px';
+    }
+
+    private getCoveredSibling() {
+        return this.dndSiblingItems()?.find((item) =>
+            isSufficientCovered(this.elem, item.elem, 50)
+        );
+    }
+
+    private setDragStartState(x: number, y: number) {
+        this.startDragY = y;
+        this.startDragX = x;
+        this.elem.style.zIndex = '999';
+        this.isStartDrag.set(true);
+    }
+
+    setStartPosition() {
+        this.elem.style.top = this.containerRelativeStartPos.top + 'px';
+        this.elem.style.left = this.containerRelativeStartPos.left + 'px';
+        this.elem.style.zIndex = '0';
+        this.isStartDrag.set(false);
     }
 }
